a2f462db733ae17d1f60cb5e35a2d548c8deed3a
hulk-56 <60963534+hulk-56@users.noreply.github.com>
2020-03-14 13:41:38 +0530

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,21 +1,15 @@
-def test_pep8(self):
-        """ runner """
-        arglist = [['statistics', True],
-                   ['show-source', True],
-                   ['repeat', True],
-                   ['paths', [BASE_DIR]]]
-
-        pep8style = pep8.StyleGuide(arglist,
-                                    parse_argv=False,
-                                    config_file=True)
-        options = pep8style.options
-        report = pep8style.check_files()
-        if options.statistics:
-            report.print_statistics()
-
-        # reporting errors (additional summary)
-        errors = report.get_count('E')
-        warnings = report.get_count('W')
-        message = 'pep8: %d errors / %d warnings' % (errors, warnings)
-        print(message)
-        assert report.total_errors == 0, message 
+class mark:
+        def user_data(self):
+                self.opt = int(input("theory mark"))
+                self.oop = int(input("theory mark"))
+                self.wp = int(input("theory mark"))
+                
+        def final_result(self):
+                self.total = self.opt+self.oop+self.wp
+                return self.total
+        
+student_1 = Mark()
+student_1.user_data()
+print(student_1.final_result())
+        
+                
